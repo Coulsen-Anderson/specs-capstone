e9a6c5c156793992a88aed74ecbea3f7057feb41
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-11-17 18:10:35 -0700

    first save on capstone

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const {SERVER_PORT} = process.env
+const {sequelize} = require('./util/database')
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+
+sequelize.sync()
+.then(() => {
+    app.listen(SERVER_PORT, console.log(`Server is running on port ${SERVER_PORT}`))
+})
+.catch(err => console.log(err))
+
---
+++ server/models/post.js
@@ -0,0 +1,17 @@
+const {DataTypes} = require('sequelize')
+
+const {sequelize} = require('../util/database')
+
+module.exports = {
+    User: sequilize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: DataTypes.STRING,
+        password: DataTypes.STRING
+
+    })
+}
