c5ab3e7a9f47f0969d029c72c90ffa414064fb7d
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-11-30 20:16:44 -0700

    Finished Project

--- src/Elements/PostItem.js
+++ src/Elements/PostItem.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./PostItem.css";
 import AuthContext from "../store/authContext";
 
-const PostItem = ({ post, getAllPosts, myPosts }) => {
+const PostItem = ({ post, getAllPosts, myposts }) => {
   const authCtx = useContext(AuthContext);
 
   const [editing, setEditing] = useState(false);
@@ -17,7 +17,15 @@ const PostItem = ({ post, getAllPosts, myPosts }) => {
 
   const updatePost = (e) => {
     e.preventDefault();
-    const body = { songTitle, album, artist, genre, comment, albumCover, Postid: post.id };
+    const body = {
+      songTitle,
+      album,
+      artist,
+      genre,
+      comment,
+      albumCover,
+      Postid: post.id,
+    };
 
     axios
       .put("/post", body)
@@ -37,86 +45,86 @@ const PostItem = ({ post, getAllPosts, myPosts }) => {
 
   console.log(post);
   return (
-  <section className="container">
-    <div className="post-card">
-      {!editing ? (
-        <div>
-          <div className="pic">
-            <img src={post.albumCover} className="albumCover"/>
+    <section className="container">
+      <div className="post-card">
+        {!editing ? (
+          <div>
+            <div className="pic">
+              <img src={post.albumCover} className="albumCover" />
+            </div>
+            {/* <p className="postElements">Song:</p> */}
+            <p className="songTitle">{post.songTitle}</p>
+            {/* <p className="postElements">Artist:</p> */}
+            <p className="artist">{post.artist}</p>
+            <p className="postElements">Album:</p>
+            <p className="postInputs">{post.album}</p>
+            <p className="postElements">Genre:</p>
+            <p className="postInputs">{post.genre}</p>
+            <p className="postInputs">{post.comment}</p>
+            <h4 className="postElements">Post made by {post.user.username}</h4>
+            {myposts ? (
+              <button className="button">Remove</button>
+            ) : (
+              <button onClick={() => saveToMyPosts()} className="button">
+                Save Post
+              </button>
+            )}
           </div>
-          {/* <p className="postElements">Song:</p> */}
-          <p className="songTitle">{post.songTitle}</p>
-          {/* <p className="postElements">Artist:</p> */}
-          <p className="artist">{post.artist}</p>
-          <p className="postElements">Album:</p>
-          <p className="postInputs">{post.album}</p>
-          <p className="postElements">Genre:</p>
-          <p className="postInputs">{post.genre}</p>
-          <p className="postInputs">{post.comment}</p>
-          <h4 className="postElements">Post made by {post.user.username}</h4>
-          {myPosts ? (
-            <button>Remove</button>
-          ) : (
-            <button onClick={() => saveToMyPosts()} className="button">Save Song</button>
-          )}
-        </div>
-        
-      
-      ) : (
-        <form onSubmit={(e) => updatePost(e)}>
-          <input
-            placeholder="Song Title"
-            type="text"
-            onChange={(e) => setSongTitle(e.target.value)}
-            value={songTitle}
-            className="input"
-          />
-          <input
-            placeholder="Album"
-            type="text"
-            onChange={(e) => setAlbum(e.target.value)}
-            value={album}
-            className="input"
-          />
-          <input
-            placeholder="Artist"
-            type="text"
-            onChange={(e) => setArtist(e.target.value)}
-            value={artist}
-            className="input"
-          />
-          <input
-            placeholder="Genre"
-            type="text"
-            onChange={(e) => setGenre(e.target.value)}
-            value={genre}
-            className="input"
-          />
-          <input
-            placeholder="Album Cover"
-            type="text"
-            onChange={(e) => setAlbumCover(e.target.value)}
-            value={albumCover}
-            className="input"
-          />
-          <input
-            placeholder="Why"
-            type="text"
-            onChange={(e) => setComment(e.target.value)}
-            value={comment}
-            className="input"
-          />
-          <button className="button">Submit</button>
-        </form>
-      )}
+        ) : (
+          <form onSubmit={(e) => updatePost(e)}>
+            <input
+              placeholder="Song Title"
+              type="text"
+              onChange={(e) => setSongTitle(e.target.value)}
+              value={songTitle}
+              className="input"
+            />
+            <input
+              placeholder="Album"
+              type="text"
+              onChange={(e) => setAlbum(e.target.value)}
+              value={album}
+              className="input"
+            />
+            <input
+              placeholder="Artist"
+              type="text"
+              onChange={(e) => setArtist(e.target.value)}
+              value={artist}
+              className="input"
+            />
+            <input
+              placeholder="Genre"
+              type="text"
+              onChange={(e) => setGenre(e.target.value)}
+              value={genre}
+              className="input"
+            />
+            <input
+              placeholder="Album Cover"
+              type="text"
+              onChange={(e) => setAlbumCover(e.target.value)}
+              value={albumCover}
+              className="input"
+            />
+            <input
+              placeholder="Why"
+              type="text"
+              onChange={(e) => setComment(e.target.value)}
+              value={comment}
+              className="input"
+            />
+            <button className="button">Submit</button>
+          </form>
+        )}
 
-      {!myPosts ? (
-        <button onClick={() => setEditing(!editing)} className="button">
-          {editing ? "Cancel Changes" : "Edit Post"}
-        </button>
-      ) : null}
-    </div>
-  </section>  
+        {!myposts ? (
+          <button onClick={() => setEditing(!editing)} className="button">
+            {editing ? "Cancel Changes" : "Edit Post"}
+          </button>
+        ) : null}
+      </div>
+    </section>
   );
 };
 
